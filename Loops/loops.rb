e172799fe30a22db03456d65418f21e2e613dcc5
babayaga24 <kumarabhishek2407@gmail.com>
2023-01-19 14:37:17 +0530

    Loops remaining assignments

--- Loops/loops.rb
+++ Loops/loops.rb
@@ -54,8 +54,101 @@
 #   puts i if i % 2 == 1
 # end
 
-friends = ['Sarah', 'John', 'Hannah', 'Dave']
+# friends = ['Sarah', 'John', 'Hannah', 'Dave']
 
-for friend in friends
-  puts "Hello #{friend}!"
+# for friend in friends
+#   puts "Hello #{friend}!"
+# end
+
+# count = 1
+
+# loop do
+#   if count % 2 == 1
+#     puts "#{count} is odd!"
+#   else
+#     puts "#{count} is even!"
+#   end
+#   count += 1
+#   break if count > 5
+# end
+
+# loop do
+#   number = rand(100)
+#   puts number
+#   break if number >= 0 && number <=10
+# end
+
+# process_the_loop = [true, false].sample
+
+# if process_the_loop 
+#   loop do
+#     puts "The loop was processed"
+#     break
+#   end
+# else
+#   puts "The loop wasn't processed"
+# end
+
+# loop do
+#   puts 'What does 2 + 2 equal?'
+#   answer = gets.chomp.to_i
+#   if answer == 4
+#     puts "That's correct!"
+#     break
+#   else puts "Wrong answer. Try again!"
+#   end
+# end
+
+# numbers = []
+
+# loop do
+#   puts 'Enter any number:'
+#   input = gets.chomp.to_i
+#   numbers << input
+#   break if numbers.size == 5
+# end
+# puts numbers
+
+# names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+# loop do
+#   puts names.shift
+#   break if names.size == 0
+# end
+
+# 5.times do |index|
+#   puts index
+#   break if index == 2
+# end
+
+# number = 0
+
+# until number == 10
+#   number += 1
+#   if number % 2 == 0
+#     puts number
+#   else number.next
+#   end
+# end
+
+# number_a = 0
+# number_b = 0
+
+# loop do
+#   number_a += rand(2)
+#   number_b += rand(2)
+#   next if number_a < 5 && number_b < 5
+#   puts "5 was reached!"
+#   break
+# end
+
+def greeting
+  puts 'Hello!'
+end
+
+number_of_greetings = 2
+
+while number_of_greetings > 0
+  greeting
+  number_of_greetings -= 1
 end
